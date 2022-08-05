72635d88cca91629f01ed2566a8df1c952170168
Amit Kumar <65532249+amitlamba12@users.noreply.github.com>
2022-08-05 19:31:01 +0530

    imageController changes

--- Controller/userImageController.js
+++ Controller/userImageController.js
@@ -18,4 +18,29 @@ const userProfile = async (req, res) => {
   }
 };
 
-module.exports = { userProfile };
+
+const multiImage = async (req, res) => {
+  const storeImags=await req.files
+  const paylodArray=storeImags.map((item)=>{
+    return {
+      name:item.originalname,
+      path:item.path ,
+      type:item.mimetype
+    }
+  })
+
+  try {
+    const userData = {
+      imageData:JSON.stringify(paylodArray),
+      price: req.body.price,
+      description: req.body.description,
+      category: req.body.category,
+    };
+    const user = await UserImageUpload.create(userData);
+
+    res.status(201).json({ userData });
+  } catch (err) {
+    return res.status(500).json("Something Went Wrong");
+  }
+};
+module.exports = { userProfile,multiImage };
