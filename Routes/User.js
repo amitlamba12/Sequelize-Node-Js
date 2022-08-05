bdc7253be4353a3e7723869a3bc711cf43f8c176
Amit Kumar <65532249+amitlamba12@users.noreply.github.com>
2022-08-05 19:36:01 +0530

    Add files via upload

--- Routes/User.js
+++ Routes/User.js
@@ -1,34 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../Controller/registeration");
-const { userProfile } = require("../Controller/userImageController");
-
-var multer = require("multer");
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname);
-  },
-});
-
-const fileFilter = (req, file, cb) => {
-  // reject a file
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-    cb(null, "upload/");
-  } else {
-    cb(null, false);
-  }
-};
-
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1024 * 1024 * 5 },
-  fileFilter: fileFilter,
-});
+const { userProfile,multiImage } = require("../Controller/userImageController");
+const {upload}=require('../uttils/multer')
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/imageData", upload.single("imageData"), userProfile);
+router.post("/multiImage", upload.array("imageData",10), multiImage);
+
 module.exports = { router };
